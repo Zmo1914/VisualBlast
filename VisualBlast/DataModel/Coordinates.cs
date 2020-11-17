52a4ae11fbe7216dd7c89e7e0c9a772f0131695b
Emil Dinev <edinev@gmail.com>
2020-11-17 22:55:51 +0200

    Create folder DataModel. Create abstract class Coordiantes. Create class DrillPoint

---
+++ VisualBlast/DataModel/Coordinates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualBlast.DataModel
+{
+    public abstract class Coordinates
+    {
+        private Coordinates coordinates;
+
+        public int PointNumber { get; set; }
+        public double PointNorthing { get; set; }
+        public double PointEasting { get; set; }
+        public double PointElevation { get; set; }
+
+        public Coordinates(int pointNumber, double pointNorthing, double pointEasting, double pointElevation)
+        {
+            PointNumber = pointNumber;
+            PointNorthing = pointNorthing;
+            PointEasting = pointEasting;
+            PointElevation = pointElevation;
+        }
+    }
+}
---
+++ VisualBlast/DataModel/DrillPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualBlast.DataModel
+{
+    class DrillPoint : Coordinates
+    {
+        public int PointDiameter { get; set; }
+        public double PointLength { get; set; }
+        public double PointSubDrill { get; set; }
+
+        public DrillPoint(int pointNumber, double pointNorthing, double pointEasting, double pointElevation, 
+            int pointDiameter, double pointLength, double pointSubDrill)
+            : base(pointNumber, pointNorthing, pointEasting, pointElevation)
+        {
+            PointDiameter = pointDiameter;
+            PointLength = pointLength;
+            PointSubDrill = pointSubDrill;
+        }
+    }
+}
