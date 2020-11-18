fd65f284c0f0eb05aa2cd049e7dfccbd3001d2f1
Emil Dinev <edinev@gmail.com>
2020-11-18 08:04:28 +0200

    Create class BlastPoint enharied DrillPoint. Create Enum DetonatorDelay.

---
+++ VisualBlast/DataModel/BlastPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualBlast.DataModel
+{
+    class BlastPoint : DrillPoint
+    {
+        public int PointExplosiveQuantity { get; set; }
+        public double PointBurden { get; set; }
+        public DetonatorDelay PointDetonatorDelay { get; set; }
+
+        public BlastPoint(int pointNumber, double pointNorthing, double pointEasting, double pointElevation,
+            int pointDiameter, double pointLength, double pointSubDrill, int pointExplosiveQuantity, double pointBurden, DetonatorDelay pointDetonatorDelay)
+            : base(pointNumber, pointNorthing, pointEasting, pointElevation,
+            pointDiameter, pointLength, pointSubDrill)
+        {
+            PointExplosiveQuantity = pointExplosiveQuantity;
+            PointBurden = pointBurden;
+            PointDetonatorDelay = pointDetonatorDelay;
+        }
+    }
+}
---
+++ VisualBlast/DataModel/DetonatorDelay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualBlast.DataModel
+{
+    enum DetonatorDelay
+    {
+        ms450=450,
+        ms475=475,
+        ms500=500,
+        ms1000=1000        
+    }
+}
